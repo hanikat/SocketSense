e48bfe819785d962d7b2e645bc0647493c94c682
Marcus Hanikat <Hanikat@kth.se>
2019-11-27 19:35:52 +0100

    Update GPIO-cleanup.py

--- test_bench/bin/GPIO-cleanup.py
+++ test_bench/bin/GPIO-cleanup.py
@@ -1,3 +1,10 @@
 import RPi.GPIO as GPIO
 
+print("Cleaning up GPIO pins...")
+GPIO.setmode(GPIO.BCM)
+for x in range(40):
+        #print("Clearing GPIO-pin nr: " + str(x))
+        GPIO.setup(x, GPIO.OUT)
+        GPIO.output(x, GPIO.LOW)
 GPIO.cleanup()
+print("Clean up done! Terminating...")
